3618d47f53654be6eecd8b834e494f1d33750518
Evert Agustin <evert.agustin@gmail.com>
2024-02-24 17:22:04 +0800

    Update images and add radio buttons for attending

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,4 @@
 $(document).ready(function () {
-    /***************** Waypoints ******************/
-
-    $('.wp1').waypoint(function () {
-        $('.wp1').addClass('animated fadeInLeft');
-    }, {
-        offset: '75%'
-    });
-    $('.wp2').waypoint(function () {
-        $('.wp2').addClass('animated fadeInRight');
-    }, {
-        offset: '75%'
-    });
-    $('.wp3').waypoint(function () {
-        $('.wp3').addClass('animated fadeInLeft');
-    }, {
-        offset: '75%'
-    });
-    $('.wp4').waypoint(function () {
-        $('.wp4').addClass('animated fadeInRight');
-    }, {
-        offset: '75%'
-    });
-    $('.wp5').waypoint(function () {
-        $('.wp5').addClass('animated fadeInLeft');
-    }, {
-        offset: '75%'
-    });
-    $('.wp6').waypoint(function () {
-        $('.wp6').addClass('animated fadeInRight');
-    }, {
-        offset: '75%'
-    });
-    $('.wp7').waypoint(function () {
-        $('.wp7').addClass('animated fadeInUp');
-    }, {
-        offset: '75%'
-    });
-    $('.wp8').waypoint(function () {
-        $('.wp8').addClass('animated fadeInLeft');
-    }, {
-        offset: '75%'
-    });
-    $('.wp9').waypoint(function () {
-        $('.wp9').addClass('animated fadeInRight');
-    }, {
-        offset: '75%'
-    });
-
-    /***************** Initiate Flexslider ******************/
-    $('.flexslider').flexslider({
-        animation: "slide"
-    });
-
-    /***************** Initiate Fancybox ******************/
-
-    $('.single_image').fancybox({
-        padding: 4
-    });
-
-    $('.fancybox').fancybox({
-        padding: 4,
-        width: 1000,
-        height: 800
-    });
-
-    /***************** Tooltips ******************/
-    $('[data-toggle="tooltip"]').tooltip();
-
     /***************** Nav Transformicon ******************/
 
     /* When user clicks the Icon */
@@ -137,35 +69,6 @@ $(document).ready(function () {
 
     });
 
-    /********************** Social Share buttons ***********************/
-    // var share_bar = document.getElementsByClassName('share-bar');
-    // var po = document.createElement('script');
-    // po.type = 'text/javascript';
-    // po.async = true;
-    // po.src = 'https://apis.google.com/js/platform.js';
-    // var s = document.getElementsByTagName('script')[0];
-    // s.parentNode.insertBefore(po, s);
-
-    // for (var i = 0; i < share_bar.length; i++) {
-    //     var html = '<iframe allowtransparency="true" frameborder="0" scrolling="no"' +
-    //         'src="https://platform.twitter.com/widgets/tweet_button.html?url=' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;via=ramswarooppatra&amp;hashtags=ramandantara&amp;count=horizontal"' +
-    //         'style="width:105px; height:21px;">' +
-    //         '</iframe>' +
-
-    //         '<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(window.location) + '&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=21&amp;appId=101094500229731&amp;width=150" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>' +
-
-    //         '<div class="g-plusone" data-size="medium"></div>';
-
-    //     // '<iframe src="https://plusone.google.com/_/+1/fastbutton?bsv&amp;size=medium&amp;url=' + encodeURIComponent(window.location) + '" allowtransparency="true" frameborder="0" scrolling="no" title="+1" style="width:105px; height:21px;"></iframe>';
-
-    //     share_bar[i].innerHTML = html;
-    //     share_bar[i].style.display = 'inline-block';
-    // }
-
-    /********************** Embed youtube video *********************/
-    // $('.player').YTPlayer();
-
-
     /********************** Toggle Map Content **********************/
     $('#btn-show-map').click(function () {
         $('#map-content').toggleClass('toggle-map-content');
@@ -178,49 +81,17 @@ $(document).ready(function () {
         $('#btn-show-direction').toggleClass('toggle-map-content');
     });
 
-    /********************** Add to Calendar **********************/
-    // var myCalendar = createCalendar({
-    //     options: {
-    //         class: '',
-    //         // You can pass an ID. If you don't, one will be generated for you
-    //         id: ''
-    //     },
-    //     data: {
-    //         // Event title
-    //         title: "Ram and Antara's Wedding",
-
-    //         // Event start date
-    //         start: new Date('Nov 27, 2017 10:00'),
-
-    //         // Event duration (IN MINUTES)
-    //         // duration: 120,
-
-    //         // You can also choose to set an end time
-    //         // If an end time is set, this will take precedence over duration
-    //         end: new Date('Nov 29, 2017 00:00'),
-
-    //         // Event Address
-    //         address: 'ITC Fortune Park Hotel, Kolkata',
-
-    //         // Event Description
-    //         description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
-    //     }
-    // });
-
-    // $('#add-to-cal').html(myCalendar);
-
-
     /********************** RSVP **********************/
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
         var data = $(this).serialize();
-
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
-
-        if (MD5($('#invite_code').val()) !== 'e3d15c611a81685d8d7d1f9a9507fe60') {
+        console.log(data);
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Please wait.</strong> We are saving your details.'));
+        
+        if (MD5($('#invite_code').val()) !== '5d129a62bb864425d402136dbeb1895a') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Oops!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyEciPb7gVwkKj7O6whDslDQC1IcQQ9iUGovNKw2bEpAR9TisiYJoZLcsJRQ6D1vqK6FQ/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbwNRaFkA5tbeKV8dTHtNddPDrulgbEqECNDJ6VfwLdIaxBDLLIN2iPfjUs1We9z9_BROQ/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -245,7 +116,8 @@ $(document).ready(function () {
         fade: true,
         cssEase: 'linear',
         autoplay: true,
-        autoplaySpeed: 3000
+        autoplaySpeed: 3000,
+        touchThreshold: 8
     });
 
 });
